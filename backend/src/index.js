7c43adfb221da421e4a7c62b2ef7ae7b98fbb588
Muhammad Uzair <abuzada735@gmail.com>
2026-05-25 21:48:12 +0500

    frontend folder and setup created

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ initializeSocket(server);
 
 connectDB ()
 .then (()=>{
-    server.listen(process.env.PORT||8000, ()=>{
+    server.listen(process.env.PORT||5000, ()=>{
         console.log( `server is running at ${process.env.PORT}`);
         
     })
